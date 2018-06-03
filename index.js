dc04a22245a5dadc655093cec480de2cf445a4bd
Edouard RUCQUOY <erucquoy@e1r2p4.42.fr>
2018-06-03 12:17:14 +0200

    dd

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ io.on('connection', (client) => {
         store.game.playersId[client.id] = count;
         store.game.players.push(client.playerData);
         
-        //client.emit('idPlayer', count);
+        client.emit('idPlayer', count);
         client.emit('joinGame', { x: client.playerData.pos.x, y: client.playerData.pos.y, direction: client.playerData.direction });
     })
 
